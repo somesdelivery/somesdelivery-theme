ae2ef471257e3d9f3710abe1e1e741c441ccba90
Dan Burzo <danburzo@gmail.com>
2017-08-02 10:24:40 +0300

    Old updates

--- front-page.php
+++ front-page.php
@@ -10,6 +10,14 @@
 
 $context = Timber::get_context();
 $post = new TimberPost();
+
+// redirect to another page if we have the redirect_to custom field
+$url = $post->redirect_to;
+if ($url) {
+	wp_redirect($url);
+	exit();
+}
+
 $context['post'] = $post;
 $context['posts'] = Timber::get_posts();
 $templates = array( 'front-page.twig', 'index.twig' );
--- page-templates/template-redirect.php
+++ page-templates/template-redirect.php
@@ -2,7 +2,7 @@
 	/* 
 		Template Name: Redirect to URL 
 		Template Post Type: post, page, editie, proiect
-	*/ 
+	*/
 
 	$post = new TimberPost();
 	$url = $post->redirect_to;
